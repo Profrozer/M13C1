a4bbc8723db776c98659b45afb323c75301558dc
Profrozer <130272328+Profrozer@users.noreply.github.com>
2023-04-23 14:32:04 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request
 import numpy as np
 import pandas as pd
@@ -10,14 +11,6 @@ app = Flask(__name__)
 def hello_world():
     request_type_str = request.method
     if request_type_str == 'GET':
-        return render_template('index.html', href2='')
+        return render_template('M13C1.html')
     else:
-        myage = request.form['age']
-        mygender = request.form['gender']
-        model = load('app/bread-recommender.joblib')
-        np_arr = np.array([myage, mygender])
-        predictions = model.predict([np_arr])  
-        predictions_to_str = str(predictions)
-        #return predictions_to_str
-        return render_template('index.html', href2='The suitable bread for you (age:'+str(myage)+' ,gender:'+str(mygender)+') is:'+predictions_to_str)
-
+        return render_template('M13C1.html')
